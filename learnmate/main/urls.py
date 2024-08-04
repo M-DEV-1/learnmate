9a546449f8ceb929696078792e4fc05e1d96f254
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2024-08-04 16:18:35 +0530

    landing page

--- learnmate/main/urls.py
+++ learnmate/main/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-    path('', views.home, name=""),
-    
-    path('dashboard', views.dashboard, name="dashboard"),
-    
+
+    path('', views.dashboard, name="dashboard"),
+    path('scheduler', views.scheduler, name='scheduler'),
+    path('', views.dashboard, name='tasks'),
+    path('assistant', views.dashboard, name='ai-assistant'),
+    path('about', views.dashboard, name='about-us')
      
     
     
--- learnmate/main/views.py
+++ learnmate/main/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/base.html')
 
 def dashboard(request):
     return render(request, 'main/dashboard.html')
+
+def scheduler(request):
+    return render(request, 'main/scheduler.html')
